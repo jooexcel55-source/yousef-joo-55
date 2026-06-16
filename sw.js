f84938f2c6221dcb7e89411667656c9a34b07c30
jooexcel55-source <jooexcel55@gmail.com>
2026-06-16 14:50:38 +0300

    Add service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('gw-v1').then(c => c.addAll(['/'])));
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
