8626f3ec2981446661b72a34c2e2f5c29c91320c
jooexcel55-source <jooexcel55@gmail.com>
2026-06-16 14:27:52 +0300

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('gw-v1').then(c => c.addAll(['/'])));
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('activate', () => {
+});
+
+self.addEventListener('fetch', () => {
 });
